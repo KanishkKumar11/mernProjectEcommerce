95b0c40380941ec935633aa0f5cf69d2dbde3f62
Kanishk Kumar <kanishkkumarr17@gmail.com>
2025-03-16 21:53:19 -0700

    Added Logger Middleware

---
+++ LoggerMiddleware.js
@@ -0,0 +1,7 @@
+// loggerMiddleware.js
+const loggerMiddleware = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next(); // Pass control to the next middleware
+};
+
+module.exports = loggerMiddleware;
